c70f8281a31e9f06233abb1b9ee814c31edc20f4
Fabian Affolter <mail@fabian-affolter.ch>
2021-02-28 19:06:40 +0100

    Add constants

---
+++ elmax/constants.py
@@ -0,0 +1,29 @@
+"""Constants for the Elmax Cloud service client."""
+
+BASE_URL = "cloud.elmaxsrl.it"
+
+ENDPOINT_DEVICES = "api/ext/devices"
+ENDPOINT_LOGIN = "api/ext/login"
+ENDPOINT_STATUS_ENTITY_ID = "api/ext/status"
+ENDPOINT_ENTITY_ID_COMMAND = "api/ext"
+ENDPOINT_DISCOVERY = "api/ext/discovery"
+
+version = "0.1.0-test-client"
+
+USER_AGENT= f"PythonElmax/{version}"
+
+COMMAND_ON = "on"
+COMMAND_OFF = "off"
+
+COMMAND_ARM = "ins"
+COMMAND_DISARM = "dis"
+
+ZONE = "zone"
+OUTPUT = "uscite"
+AREA = "aree"
+
+UNIT_TYPES = [
+    AREA,
+    OUTPUT,
+    ZONE,
+]
